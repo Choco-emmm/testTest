db4a2f75ae9b775957beac3f7e0a60b25dbb2400
luseeee-star <13726466057@163.com>
2026-07-18 16:14:24 +0800

    app.js

--- app.js
+++ app.js
@@ -8,11 +8,12 @@ function sbdhx() {
 
 }
 
-//云玩家崽崽
-function cloud() {
-	let cloud
-  cloud = 'cloud' // 那就乱写
-  console.log("Git push test");
-}
+
 
 // 123123
+
+function dhxbaby() {
+	let cloud
+  cloud = 'dhx' // 那就乱写
+  console.log("Git push test");
+}
